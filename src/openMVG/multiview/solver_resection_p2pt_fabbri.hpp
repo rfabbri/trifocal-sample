c5f3f646cf1108d60149829cfe47cd4d73dbb8e7
Ricardo Fabbri <rfabbri@gmail.com>
2022-05-08 01:43:02 -0300

    [P2Pt] resection hpp

--- src/openMVG/multiview/solver_resection_p2pt_fabbri.hpp
+++ src/openMVG/multiview/solver_resection_p2pt_fabbri.hpp
@@ -1,42 +1,51 @@
 // This file is part of OpenMVG, an Open Multiple View Geometry C++ library.
 
-// Copyright (c) 2020 Pierre MOULON.
+// Copyright (c) 2020 Ricardo Fabbri
+// Adapted to openMVG by Ariel Kovaljski and Ricardo Fabbri
 
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-#ifndef OPENMVG_MULTIVIEW_RESECTION_UP2P_HPP
-#define OPENMVG_MULTIVIEW_RESECTION_UP2P_HPP
+#ifndef OPENMVG_MULTIVIEW_RESECTION_P2Pt_FABBRI_HPP
+#define OPENMVG_MULTIVIEW_RESECTION_P2Pt_FABBRI_HPP
 
 #include "openMVG/multiview/two_view_kernel.hpp"
 #include "openMVG/multiview/solver_resection_metrics.hpp"
 
-namespace openMVG {
-namespace euclidean_resection {
+namespace openMVG
+{
+namespace euclidean_resection
+{
 
-struct P2PtSolver_Fabbri {
-  enum { MINIMUM_SAMPLES = 2 };
-  enum { MAX_MODELS = 2 };
+struct P2PtSolver_Fabbri
+{
+  enum
+  {
+    MINIMUM_SAMPLES = 2
+  };
+  enum
+  {
+    MAX_MODELS = 8
+  };
 
-  // Solve the absolute camera pose problem for the p2p setting (SIFT features)
-  // [1] 
-  static void Solve
-  (
-    const Mat & bearing_vectors,
-    const Mat & X, // 3D points
-    std::vector<Mat34> *models
-  );
+  // Solve for absolute camera pose from 2 points with (SIFT) orientation
+  // Paper: Camera Pose Estimation Using Curve Differential Geometry, 
+  // IEEE Transactions on Pattern Analysis and Machine Intelligence, 2020, 
+  // Ricardo Fabbri, Peter J. Giblin and Benjamin Kimia
+  static void Solve(
+      const Mat &bearing_vectors,
+      const Mat &X, // 3D points
+      std::vector<Mat34> *models);
 };
 
 //-- Usable solver for robust estimation framework
-using PoseResectionKernel_P2Pt_Fabbri =
-  two_view::kernel::Kernel<
-    P2PtSolver_Fabbri, // Model estimator
-    resection::AngularReprojectionError, // Error metric
-    Mat34>;
+using PoseResectionKernel_P2Pt_FABBRI =
+    two_view::kernel::Kernel<
+        P3PSolver_Nordberg, // Model estimator
+        resection::AngularReprojectionError, // Error metric
+        Mat34>;
 
-}  // namespace euclidean_resection
-}  // namespace openMVG
+} // namespace euclidean_resection
+} // namespace openMVG
 
-#endif // OPENMVG_MULTIVIEW_RESECTION_UP2P_HPP
+#endif // OPENMVG_MULTIVIEW_RESECTION_P2Pt_FABBRI_HPP
